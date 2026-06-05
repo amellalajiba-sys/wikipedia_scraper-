09c39f6a7d2edbdf4b67364f85c9653776e3bc98
unknown <amellal.ajiba@gmail.com>
2026-06-05 15:39:37 +0200

    Added comments and changed some

--- src/api_client.py
+++ src/api_client.py
@@ -8,18 +8,19 @@ class CountryLeadersAPI:
         self.cookie_url = self.base_url + "/cookie"
         self.countries_url = self.base_url + "/countries"
         self.leaders_url = self.base_url + "/leaders"
-        self.cookies = None#None at start because we don't have a cookie yet.
+        self.cookies = None #None at start because we don't have a cookie yet.
 
     def refresh_cookie(self):
         #This ask the api for a fresh cookie because the api is protected and the cookie expire fast.
         self.cookies = requests.get(self.cookie_url).cookies
-
+        # We send a get request to /cookie, the API sends me a cookie and we stock it in self.cookies
     def get_countries(self):
         #If we don't have a cookie yet we get one first before touching the protected endpoint.
         if self.cookies is None:
             self.refresh_cookie()
+            # We do that because as we said the cookie expires quickly so it fetches a new one.
 
-        #We ask the api the list of countries like fr be us etc.
+        #We ask the api the list of countries using the cookie.
         response = requests.get(self.countries_url, cookies=self.cookies)
 
         #If the cookie expired or something failed, we retry once with a fresh cookie.
@@ -31,17 +32,19 @@ class CountryLeadersAPI:
         return response.json()
 
     def get_leaders(self, country):
-        #Same logic as get_countries but this time for one country at a time.
+        # Fetches the list of political leaders for a given country from the API..
         if self.cookies is None:
             self.refresh_cookie()
+            #Same logic as above: No cookie, we retry with a fresh one
 
-        #params add ?country=fr or ?country=be etc at the end of the url.
+        #We call leaders: with a cookie and with a url parameter like ?country=fr.
         response = requests.get(self.leaders_url, cookies=self.cookies, params={"country": country})
 
-        #If the cookie expired while looping we refresh and retry once.
+        .
         if response.status_code != 200:
+            #If the request fails, with retry it with a fresh cookie.
             self.refresh_cookie()
             response = requests.get(self.leaders_url, cookies=self.cookies, params={"country": country})
 
-        #This should give back the list of leaders for that country.
+        #You return the list of leaders as a Python object: a list of dictionaries.
         return response.json()
